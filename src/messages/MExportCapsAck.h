824c0ffbf0bc3c77f36cf227d77f590973ebb050
dongdong tao <tdd21151186@gmail.com>
2018-02-04 13:32:04 +0800

    mds: make MExportCaps/MExportCapsAck versioned
    Signed-off-by: dongdong tao <tdd21151186@gmail.com>

--- src/messages/MExportCapsAck.h
+++ src/messages/MExportCapsAck.h
@@ -20,13 +20,16 @@
 
 
 class MExportCapsAck : public Message {
+ static const int HEAD_VERSION = 1;
+ static const int COMPAT_VERSION = 1;
+
  public:  
   inodeno_t ino;
 
   MExportCapsAck() :
-    Message(MSG_MDS_EXPORTCAPSACK) {}
+    Message(MSG_MDS_EXPORTCAPSACK, HEAD_VERSION, COMPAT_VERSION) {}
   MExportCapsAck(inodeno_t i) :
-    Message(MSG_MDS_EXPORTCAPSACK), ino(i) {}
+    Message(MSG_MDS_EXPORTCAPSACK, HEAD_VERSION, COMPAT_VERSION), ino(i) {}
 private:
   ~MExportCapsAck() override {}
 
