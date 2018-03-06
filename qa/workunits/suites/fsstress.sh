0982acc7c837104fd51e72387d8469cf2098f962
Patrick Donnelly <pdonnell@redhat.com>
2018-03-06 14:57:51 -0800

    qa: cleanup fsstress bash wrapper
    Makes the output easier to read.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/workunits/suites/fsstress.sh
+++ qa/workunits/suites/fsstress.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
+function run {
+  printf '%s\n' "$*" >&2
+  "$@"
+}
+
 BIN_PATH=${TESTDIR}/fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress/fsstress
 
-path=`pwd`
 trap "rm -rf ${TESTDIR}/fsstress" EXIT
-mkdir -p ${TESTDIR}/fsstress
-cd ${TESTDIR}/fsstress
-wget -q -O ${TESTDIR}/fsstress/ltp-full.tgz http://download.ceph.com/qa/ltp-full-20091231.tgz
-tar xzf ${TESTDIR}/fsstress/ltp-full.tgz
-rm ${TESTDIR}/fsstress/ltp-full.tgz
-cd ${TESTDIR}/fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress
-make
-cd $path
+run mkdir -p ${TESTDIR}/fsstress
+run pushd "${TESTDIR}/fsstress"
+run wget -q -O fsstress/ltp-full.tgz http://download.ceph.com/qa/ltp-full-20091231.tgz
+run tar xzf fsstress/ltp-full.tgz
+run rm fsstress/ltp-full.tgz
+run cd fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress
+run make
+run popd
 
-command="${BIN_PATH} -d fsstress-`hostname`$$ -l 1 -n 1000 -p 10 -v"
-
-echo "Starting fsstress $command"
-mkdir fsstress`hostname`-$$
-$command
+printf "Starting fsstress...\n" >&2
+DIR="fsstress-$(hostname)-$$"
+run mkdir -- "$DIR"
+run "${BIN_PATH}" -d "$DIR" -l 1 -n 1000 -p 10 -v
