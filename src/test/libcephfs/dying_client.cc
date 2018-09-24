3473c356bc2746aeafe0119de7618aee5ddbd9d9
Jeff Layton <jlayton@redhat.com>
2018-09-24 10:47:34 -0400

    test: don't use assert() in dying_client program
    assert() can be compiled out in some situations, which means that the
    internal bit doesn't get run. Just open code the error handling the the
    dying_client program.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/test/libcephfs/dying_client.cc
+++ src/test/libcephfs/dying_client.cc
@@ -6,26 +6,8 @@
  * (c) 2017, Jeff Layton <jlayton@redhat.com>
  */
 
-#include "gtest/gtest.h"
 #include "include/cephfs/libcephfs.h"
-#include "include/stat.h"
 #include <errno.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <sys/xattr.h>
-#include <sys/uio.h>
-
-#ifdef __linux__
-#include <limits.h>
-#endif
-
-#include <map>
-#include <vector>
-#include <thread>
-#include <atomic>
 
 #define	CEPHFS_RECLAIM_TIMEOUT		60
 
@@ -34,25 +16,42 @@ int main(int argc, char **argv)
   struct ceph_mount_info *cmount;
 
   /* Caller must pass in the uuid */
-  assert(argc >= 1);
+  if (argc < 2)
+    return 1;
 
-  assert(ceph_create(&cmount, NULL) == 0);
-  assert(ceph_conf_read_file(cmount, NULL) == 0);
-  assert(ceph_conf_parse_env(cmount, NULL) == 0);
-  assert(ceph_init(cmount) == 0);
+  if (ceph_create(&cmount, nullptr) != 0)
+    return 1;
+
+  if (ceph_conf_read_file(cmount, nullptr) != 0)
+    return 1;
+
+  if (ceph_conf_parse_env(cmount, nullptr) != 0)
+    return 1;
+
+  if (ceph_init(cmount) != 0)
+    return 1;
+
   ceph_set_session_timeout(cmount, CEPHFS_RECLAIM_TIMEOUT);
-  assert(ceph_start_reclaim(cmount, argv[1], CEPH_RECLAIM_RESET) == -ENOENT);
+
+  if (ceph_start_reclaim(cmount, argv[1], CEPH_RECLAIM_RESET) != -ENOENT)
+    return 1;
+
   ceph_set_uuid(cmount, argv[1]);
-  assert(ceph_mount(cmount, "/") == 0);
+
+  if (ceph_mount(cmount, "/") != 0)
+    return 1;
 
   Inode *root, *file;
-  assert(ceph_ll_lookup_root(cmount, &root) == 0);
+  if (ceph_ll_lookup_root(cmount, &root) != 0)
+    return 1;
 
   Fh *fh;
   struct ceph_statx stx;
   UserPerm *perms = ceph_mount_perms(cmount);
 
-  assert(ceph_ll_create(cmount, root, argv[1], 0666,
-	    O_RDWR|O_CREAT|O_EXCL, &file, &fh, &stx, 0, 0, perms) == 0);
+  if (ceph_ll_create(cmount, root, argv[1], 0666, O_RDWR|O_CREAT|O_EXCL,
+		      &file, &fh, &stx, 0, 0, perms) != 0)
+    return 1;
+
   return 0;
 }
