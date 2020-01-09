ed5c820872d5b8a81ae03f83f4e5149864f8686e
Patrick Donnelly <pdonnell@redhat.com>
2020-01-09 15:18:51 -0800

    mds: use max_mds for denying scrub
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/mds/MDSRank.cc
+++ src/mds/MDSRank.cc
@@ -2385,11 +2385,9 @@ void MDSRankDispatcher::handle_mds_map(
     dout(0) << "WARNING: inline_data support has been deprecated and will be removed in a future release" << dendl;
 
   if (scrubstack->is_scrubbing()) {
-    unsigned active = mdsmap->get_num_mds(MDSMap::DaemonState::STATE_ACTIVE);
-    unsigned stopping = mdsmap->get_num_mds(MDSMap::DaemonState::STATE_STOPPING);
-    if (active + stopping > 1) {
-      C_MDSInternalNoop cond;
-      scrubstack->scrub_abort(&cond);
+    if (mdsmap->get_max_mds() > 1) {
+      auto c = new C_MDSInternalNoop;
+      scrubstack->scrub_abort(c);
     }
   }
   mdcache->handle_mdsmap(*mdsmap);
@@ -2526,15 +2524,12 @@ void MDSRankDispatcher::handle_asok_command(
     cmd_getval(g_ceph_context, cmdmap, "scrubops", scrubop_vec);
     cmd_getval(g_ceph_context, cmdmap, "path", path);
     cmd_getval(g_ceph_context, cmdmap, "tag", tag);
+
     /* if there are more than one mds active and a recursive scrub is requested,
      * dishonor the request
      */
-    unsigned active = mdsmap->get_num_mds(MDSMap::DaemonState::STATE_ACTIVE);
-    unsigned stopping = mdsmap->get_num_mds(MDSMap::DaemonState::STATE_STOPPING);
-    bool is_recursive = std::find(scrubop_vec.begin(), scrubop_vec.end(), string("recursive")) != scrubop_vec.end();
-    // if (mdsmap->get_max_mds() > 1 &&
-    if ((active + stopping > 1) && is_recursive) {
-      ss << "There are more than one MDS active. Hence a recursive scrub "
+    if (mdsmap->get_max_mds() > 1) {
+      ss << "There is more than one MDS active. Hence a recursive scrub "
             "request cannot be started.";
       r = -EINVAL;
       goto out;
