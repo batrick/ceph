bdb9dcfa0ed09d786dd089ff07c397b313ba5ee2
Kyr Shatskyy <kyrylo.shatskyy@suse.com>
2020-08-04 19:54:15 +0200

    qa/tasks/cephfs: use remote.write_file
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@suse.com>

--- qa/tasks/cephfs/test_volumes.py
+++ qa/tasks/cephfs/test_volumes.py
@@ -10,7 +10,6 @@ from hashlib import md5
 
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
 from teuthology.exceptions import CommandFailedError
-from teuthology.misc import sudo_write_file
 
 log = logging.getLogger(__name__)
 
@@ -286,7 +285,8 @@ class TestVolumes(CephFSTestCase):
             # add a fake clone source
             meta_contents = meta_contents + '[source]\nvolume = fake\nsubvolume = fake\nsnapshot = fake\n'
         meta_filepath1 = os.path.join(self.mount_a.mountpoint, basepath, ".meta")
-        sudo_write_file(self.mount_a.client_remote, meta_filepath1, meta_contents)
+        self.mount_a.client_remote.write_file(meta_filepath1,
+                                              meta_contents, sudo=True)
         return createpath
 
     def setUp(self):
