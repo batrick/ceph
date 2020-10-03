7e1fb651653a5d85e0ed6abef1cd525eb4b3f95a
Patrick Donnelly <pdonnell@redhat.com>
2020-10-02 17:11:54 -0700

    osdc: add timeout configs for mons/osds
    The Monitors may not respond to invalid requests like statfs on a
    non-existent pool. In this case, the Objecter should eventually give up
    rather than block forever.
    
    Fixes: https://tracker.ceph.com/issues/47734
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit ccc6d6cefcd4235c8a7aa4120a0622c2f17e5a38)
    
    Conflicts:
    	src/osdc/Objecter.cc

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -167,11 +167,6 @@ enum {
 
 // config obs ----------------------------
 
-static const char *config_keys[] = {
-  "crush_location",
-  NULL
-};
-
 class Objecter::RequestStateHook : public AdminSocketHook {
   Objecter *m_objecter;
 public:
@@ -232,6 +227,12 @@ Objecter::OSDSession::unique_completion_lock Objecter::OSDSession::get_lock(
 
 const char** Objecter::get_tracked_conf_keys() const
 {
+  static const char *config_keys[] = {
+    "crush_location",
+    "osdc_mon_timeout",
+    "osdc_osd_timeout",
+    NULL
+  };
   return config_keys;
 }
 
@@ -241,6 +242,16 @@ void Objecter::handle_conf_change(const ConfigProxy& conf,
 {
   if (changed.count("crush_location")) {
     update_crush_location();
+  }
+  if (changed.count("osdc_mon_timeout")) {
+    if (!mon_timeout_configured) {
+      mon_timeout = conf.get_val<std::chrono::seconds>("osdc_mon_timeout");
+    }
+  }
+  if (changed.count("osdc_osd_timeout")) {
+    if (!osd_timeout_configured) {
+      osd_timeout = conf.get_val<std::chrono::seconds>("osdc_osd_timeout");
+    }
   }
 }
 
@@ -4963,19 +4974,32 @@ Objecter::OSDSession::~OSDSession()
 
 Objecter::Objecter(CephContext *cct_, Messenger *m, MonClient *mc,
 		   Finisher *fin,
-		   double mon_timeout,
-		   double osd_timeout) :
+		   double mon_timeout_,
+		   double osd_timeout_) :
   Dispatcher(cct_), messenger(m), monc(mc), finisher(fin),
   trace_endpoint("0.0.0.0", 0, "Objecter"),
   osdmap{std::make_unique<OSDMap>()},
   homeless_session(new OSDSession(cct, -1)),
-  mon_timeout(ceph::make_timespan(mon_timeout)),
-  osd_timeout(ceph::make_timespan(osd_timeout)),
+  mon_timeout(ceph::make_timespan(mon_timeout_)),
+  osd_timeout(ceph::make_timespan(osd_timeout_)),
   op_throttle_bytes(cct, "objecter_bytes",
 		    cct->_conf->objecter_inflight_op_bytes),
   op_throttle_ops(cct, "objecter_ops", cct->_conf->objecter_inflight_ops),
   retry_writes_after_first_reply(cct->_conf->objecter_retry_writes_after_first_reply)
-{}
+{
+  ceph_assert(mon_timeout_ >= 0.0);
+  if (mon_timeout_ > 0.0) {
+    mon_timeout_configured = true;
+  } else {
+    mon_timeout = cct->_conf.get_val<std::chrono::seconds>("osdc_mon_timeout");
+  }
+  ceph_assert(osd_timeout_ >= 0.0);
+  if (osd_timeout_ > 0.0) {
+    osd_timeout_configured = true;
+  } else {
+    osd_timeout = cct->_conf.get_val<std::chrono::seconds>("osdc_osd_timeout");
+  }
+}
 
 Objecter::~Objecter()
 {
