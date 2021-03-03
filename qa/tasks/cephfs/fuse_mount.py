08f4a2fe8784bcee8680f7bbcc8d730bcc85bb88
Patrick Donnelly <pdonnell@redhat.com>
2021-03-02 18:38:36 -0800

    qa: move get_valgrind_args to qa
    This method is unused in the teuthology repo. The helper method better
    belongs here where it is more easily modified.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/tasks/cephfs/fuse_mount.py
+++ qa/tasks/cephfs/fuse_mount.py
@@ -10,6 +10,7 @@ from teuthology.contextutil import MaxWhileTries
 from teuthology.contextutil import safe_while
 from teuthology.orchestra import run
 from teuthology.orchestra.run import CommandFailedError
+from tasks.ceph_manager import get_valgrind_args
 from tasks.cephfs.mount import CephFSMount
 
 log = logging.getLogger(__name__)
@@ -95,13 +96,13 @@ class FuseMount(CephFSMount):
 
         cwd = self.test_dir
         if self.client_config.get('valgrind') is not None:
-            run_cmd = misc.get_valgrind_args(
+            run_cmd = get_valgrind_args(
                 self.test_dir,
                 'client.{id}'.format(id=self.client_id),
                 run_cmd,
                 self.client_config.get('valgrind'),
             )
-            cwd = None # misc.get_valgrind_args chdir for us
+            cwd = None # get_valgrind_args chdir for us
 
         netns_prefix = ['sudo', 'nsenter',
                         '--net=/var/run/netns/{0}'.format(self.netns_name)]
