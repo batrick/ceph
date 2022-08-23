batrick/ceph

commit c9cfaef104e9aaefad55583d7e54f8b4665904b3

mds: catch duplicates in DamageTable
There was an implicit assumption in the code that callers
wouldn't hit the notify_*damaged paths twice because they would
have checked is_*_damaged paths first.

However, that's not really true in all cases, e.g. scrub
code isn't required to respect existing damage entries
when trying to load a CDir.

Simply fix this by having the DamageTable notify* functions
check the key they're inserting doesn't already exist.

Fixes: http://tracker.ceph.com/issues/17173
Signed-off-by: John Spray <john.spray@redhat.com>

src/mds/DamageTable.cc
@@ -46,10 +46,13 @@ bool DamageTable::notify_dentry(
     return true;
   }
 
-  DamageEntryRef entry = std::make_shared<DentryDamage>(
-      ino, frag, dname, snap_id);
-  dentries[DirFragIdent(ino, frag)][DentryIdent(dname, snap_id)] = entry;
-  by_id[entry->id] = entry;
+  auto key = DirFragIdent(ino, frag);
+  if (dentries.count(key) == 0) {
+    DamageEntryRef entry = std::make_shared<DentryDamage>(
+        ino, frag, dname, snap_id);
+    dentries[key][DentryIdent(dname, snap_id)] = entry;
+    by_id[entry->id] = entry;
+  }
 
   return false;
 }
@@ -72,9 +75,12 @@ bool DamageTable::notify_dirfrag(inodeno_t ino, frag_t frag)
     return true;
   }
 
-  DamageEntryRef entry = std::make_shared<DirFragDamage>(ino, frag);
-  dirfrags[DirFragIdent(ino, frag)] = entry;
-  by_id[entry->id] = entry;
+  auto key = DirFragIdent(ino, frag);
+  if (dirfrags.count(key) == 0) {
+    DamageEntryRef entry = std::make_shared<DirFragDamage>(ino, frag);
+    dirfrags[key] = entry;
+    by_id[entry->id] = entry;
+  }
 
   return false;
 }
@@ -85,9 +91,11 @@ bool DamageTable::notify_remote_damaged(inodeno_t ino)
     return true;
   }
 
-  auto entry = std::make_shared<BacktraceDamage>(ino);
-  remotes[ino] = entry;
-  by_id[entry->id] = entry;
+  if (remotes.count(ino) == 0) {
+    auto entry = std::make_shared<BacktraceDamage>(ino);
+    remotes[ino] = entry;
+    by_id[entry->id] = entry;
+  }
 
   return false;
 }

commit d4eff26cc7a5d8aefe9543ae688085492dc66b97

mds: correct storage of multiple damaged dentries in DamageTable
Fixes: c9cfaef104e9aaefad55583d7e54f8b4665904b3
Fixes: https://tracker.ceph.com/issues/57249
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/DamageTable.cc
@@ -147,12 +147,11 @@ bool DamageTable::notify_dentry(
     return true;
   }
 
-  auto key = DirFragIdent(ino, frag);
-  if (dentries.count(key) == 0) {
-    DamageEntryRef entry = std::make_shared<DentryDamage>(
-        ino, frag, dname, snap_id);
+  auto& df_dentries = dentries[DirFragIdent(ino, frag)];
+  if (auto [it, inserted] = df_dentries.try_emplace(DentryIdent(dname, snap_id)); inserted) {
+    auto entry = std::make_shared<DentryDamage>(ino, frag, dname, snap_id);
     entry->path = path;
-    dentries[key][DentryIdent(dname, snap_id)] = entry;
+    it->second = entry;
     by_id[entry->id] = std::move(entry);
   }
 
@@ -175,11 +174,10 @@ bool DamageTable::notify_dirfrag(inodeno_t ino, frag_t frag,
     return true;
   }
 
-  auto key = DirFragIdent(ino, frag);
-  if (dirfrags.count(key) == 0) {
+  if (auto [it, inserted] = dirfrags.try_emplace(DirFragIdent(ino, frag)); inserted) {
     DamageEntryRef entry = std::make_shared<DirFragDamage>(ino, frag);
     entry->path = path;
-    dirfrags[key] = entry;
+    it->second = entry;
     by_id[entry->id] = std::move(entry);
   }
 
@@ -192,10 +190,10 @@ bool DamageTable::notify_remote_damaged(inodeno_t ino, std::string_view path)
     return true;
   }
 
-  if (remotes.count(ino) == 0) {
+  if (auto [it, inserted] = remotes.try_emplace(ino); inserted) {
     auto entry = std::make_shared<BacktraceDamage>(ino);
     entry->path = path;
-    remotes[ino] = entry;
+    it->second = entry;
     by_id[entry->id] = std::move(entry);
   }
 