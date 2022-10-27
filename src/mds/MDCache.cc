batrick/ceph

commit 138fea6a7638697acb1a9e824db7b8d04ad8d671

mds: do not trim cache when creating system file
Fixes: https://tracker.ceph.com/issues/51023
Signed-off-by: Xiubo Li <xiubli@redhat.com>

src/mds/MDCache.cc
@@ -13273,7 +13273,9 @@ void MDCache::upkeep_main(void)
         if (active_with_clients) {
           trim_client_leases();
         }
-        trim();
+        if (is_open()) {
+          trim();
+        }
         if (active_with_clients) {
           auto recall_flags = Server::RecallFlags::ENFORCE_MAX|Server::RecallFlags::ENFORCE_LIVENESS;
           if (cache_toofull()) {

commit 8f59210d14f4287fa69fd5989f9b9210d09b48b8

mds: trim cache during standby-replay
Fixes: 138fea6a7638697acb1a9e824db7b8d04ad8d671
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/MDCache.cc
@@ -13549,7 +13549,7 @@ void MDCache::upkeep_main(void)
         if (active_with_clients) {
           trim_client_leases();
         }
-        if (is_open()) {
+        if (is_open() || mds->is_standby_replay()) {
           trim();
         }
         if (active_with_clients) {