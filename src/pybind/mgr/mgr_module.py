5b0183c7240836a728fc118637e4191b42890990
Patrick Donnelly <pdonnell@redhat.com>
2023-02-27 15:00:08 -0500

    mgr: rework register_client to take name and replace
    Two new parameters:
    
    - "name" to allow setting the name associated with the addrv. Previously
      this was hard-coded to the mgr module name. However, we want mgr
      modules to be able to update the libcephsqlite cluster handle.
    
    - "replace" to replace all addrv for the given "name". libcephsqlite
      only ever has one handle, remove the old ones.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/pybind/mgr/mgr_module.py
+++ src/pybind/mgr/mgr_module.py
@@ -1329,7 +1329,7 @@ class MgrModule(ceph_module.BaseMgrModule, MgrModuleLoggingMixin):
         if self._rados:
             addrs = self._rados.get_addrs()
             self._rados.shutdown()
-            self._ceph_unregister_client(addrs)
+            self._ceph_unregister_client(None, addrs)
 
     @API.expose
     def get(self, data_name: str) -> Any:
@@ -2135,7 +2135,7 @@ class MgrModule(ceph_module.BaseMgrModule, MgrModuleLoggingMixin):
         ctx_capsule = self.get_context()
         self._rados = rados.Rados(context=ctx_capsule)
         self._rados.connect()
-        self._ceph_register_client(self._rados.get_addrs())
+        self._ceph_register_client(None, self._rados.get_addrs(), False)
         return self._rados
 
     @staticmethod
