0b8d97696ffad58877015ab649a4be5205f04246
Patrick Donnelly <pdonnell@redhat.com>
2023-03-21 16:41:17 -0400

    qa/tasks: use best compression
    To save space on the LRC.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/tasks/rook.py
+++ qa/tasks/rook.py
@@ -279,6 +279,7 @@ def ceph_log(ctx, config):
                         '--no-run-if-empty',
                         '--',
                         'gzip',
+                        '--best',
                         '--',
                     ],
                     wait=False,
