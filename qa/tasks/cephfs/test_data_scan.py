e31a62f6d7532fca7aa3d6e26ea27839753e9595
Patrick Donnelly <pdonnell@ibm.com>
2025-01-29 21:05:35 -0500

    initfix

--- qa/tasks/cephfs/test_data_scan.py
+++ qa/tasks/cephfs/test_data_scan.py
@@ -499,7 +499,7 @@ class TestDataScan(CephFSTestCase):
                 self.fs.journal_tool(["journal", "reset", "--yes-i-really-really-mean-it"], 0)
 
         self.fs.journal_tool(["journal", "reset", "--force", "--yes-i-really-really-mean-it"], 0)
-        self.fs.data_scan(["init"])
+        self.fs.data_scan(["init", "--force-init"])
         self.fs.data_scan(["scan_extents"], worker_count=workers)
         self.fs.data_scan(["scan_inodes"], worker_count=workers)
         self.fs.data_scan(["scan_links"])
