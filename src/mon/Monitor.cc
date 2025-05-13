4e56aafd2079c9d18e49f2f5c5236a7b7b7eb539
Patrick Donnelly <pdonnell@ibm.com>
2025-05-13 12:26:48 -0400

    msg: add interface to shutdown Connection
    Unfortunately this doesn't work as-is because I couldn't find primitives to
    flush the out_queue. It's left as a to-do for now.
    
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
    Change-Id: 71913c06-ba62-44f5-aeec-a92e0c8182ad

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -4272,6 +4272,9 @@ struct AnonConnection : public Connection {
   void mark_down() override {
     // silently ignore
   }
+  void shutdown() override {
+    // silengtly ignore
+  }
   void mark_disposable() override {
     // silengtly ignore
   }
