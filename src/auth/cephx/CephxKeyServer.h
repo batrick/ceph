f7e1511c467e9e98a289451dcc747452c5ee2773
Patrick Donnelly <pdonnell@ibm.com>
2025-05-29 10:01:37 -0400

    auth/cephx: make some parameters const
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

--- src/auth/cephx/CephxKeyServer.h
+++ src/auth/cephx/CephxKeyServer.h
@@ -94,7 +94,7 @@ struct KeyServerData {
     rotating_secrets.clear();
   }
 
-  void add_auth(const EntityName& name, EntityAuth& auth) {
+  void add_auth(const EntityName& name, const EntityAuth& auth) {
     secrets[name] = auth;
   }
 
@@ -302,7 +302,7 @@ public:
     data.version = ver;
   }
 
-  void add_auth(const EntityName& name, EntityAuth& auth) {
+  void add_auth(const EntityName& name, const EntityAuth& auth) {
     std::scoped_lock l{lock};
     data.add_auth(name, auth);
   }
