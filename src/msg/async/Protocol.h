batrick/ceph

commit d599bd174cb693a15069fe3b427d1c26c90b5639

msg/async/Protocol: include cleanup
Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

src/msg/async/Protocol.h
@@ -4,10 +4,7 @@
 #ifndef _MSG_ASYNC_PROTOCOL_
 #define _MSG_ASYNC_PROTOCOL_
 
-#include <list>
-#include <map>
-
-#include "AsyncConnection.h"
+#include "auth/Auth.h"
 #include "include/buffer.h"
 #include "include/msgr.h"
 
@@ -17,6 +14,9 @@
 
 #include <memory>
 #include <tuple>
+
+class AsyncConnection;
+class Message;
 
 template <class C>
 class Ct {

commit 6c9e26dcbb919c37c53e89173a2f3ccf58414a39

msg/async: include MessageRef dependency
Fixes: d599bd174cb693a15069fe3b427d1c26c90b5639
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
Change-Id: c1e67590-3e8d-460b-874b-dcd1c3a916c6

src/msg/async/Protocol.h
@@ -7,6 +7,7 @@
 #include "auth/Auth.h"
 #include "include/buffer.h"
 #include "include/msgr.h"
+#include "msg/MessageRef.h"
 
 /*
  * Continuation Helper Classes
@@ -16,7 +17,6 @@
 #include <tuple>
 
 class AsyncConnection;
-class Message;
 
 template <class C>
 class Ct {